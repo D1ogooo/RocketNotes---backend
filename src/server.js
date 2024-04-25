1402c71e3578da5cff71affa87a0a1679088d803
D1ogooo <diiogocosta11@gmail.com>
2024-04-25 14:17:18 -0300

    Conexão com banco de dados estabelecida

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 require('express-async-errors')
 const AppError = require('./utils/AppError') 
+const database = require('./database/sqlite')
 const express = require('express')
 const routes = require('./routes')
 
 const app = express();
 app.use(express.json());
-
 app.use(routes);
+database()
 
 app.use((error, req, res, next) => {
   if(error instanceof AppError) {
