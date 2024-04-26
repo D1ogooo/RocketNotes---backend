cf84367020eba1745dc92895772ff5042231c148
D1ogooo <diiogocosta11@gmail.com>
2024-04-26 15:02:00 -0300

    Migrations concluida

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,8 @@
-class UsersController {
+class usersController {
  create(req,res) {
   const { name, email, password } = req.body;
   res.status(201).json({name, email , password});
  }
 }
 
-module.exports = UsersController;
+module.exports = usersController;
