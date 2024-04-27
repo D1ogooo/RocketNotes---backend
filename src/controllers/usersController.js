ba3a9ce76ecbb15749451a65624b399523f332d5
D1ogooo <diiogocosta11@gmail.com>
2024-04-26 23:51:09 -0300

    Cadastro de usuários implementado no banco dedados

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,7 +1,24 @@
+const { hash } = require('bcryptjs')
+const sqliteConnection = require("../database/sqlite");
+const AppError = require("../utils/AppError");
+
 class usersController {
- create(req,res) {
+ async create(req,res) {
   const { name, email, password } = req.body;
-  res.status(201).json({name, email , password});
+
+  const database = await sqliteConnection();
+  const checkUserExists = await database.get(`SELECT * FROM users WHERE email = (?)`, [email])
+  if(checkUserExists) {
+   throw new AppError("Este email já está em uso.");
+  }
+
+ const  hashedPassword = await hash(password, 8);
+ 
+ await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+ [name, email, hashedPassword]
+)
+
+  return res.status(201).json();
  }
 }
 
