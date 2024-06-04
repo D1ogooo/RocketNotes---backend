7b85ddd7e35ffbffe96ef6f60029835b575c26fd
D1ogooo <diiogocosta11@gmail.com>
2024-06-04 06:18:18 -0300

    Sessions com jwt feito.

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,5 +1,8 @@
 const knex = require("../database/knex");
 const AppError = require('../utils/AppError')
+const { compare } = require('bcryptjs')
+const authConfig = require('../configs/auth')
+const { sign } = require('jsonwebtoken')
 
 class SessionsController {
   async create(req,res) {
@@ -9,8 +12,19 @@ class SessionsController {
    if(!user) {
     throw new AppError("E-mail e/ou senha incorreta", 401)
    }
+   const passwordMatched = await compare(password, user.password);
 
-   return res.json(email, password)
+   if(!passwordMatched) {
+    throw new AppError("E-mail e/ou senha incorreta", 401)
+   }
+
+   const { secret, expiresIn } = authConfig.jwt;
+   const token = sign({}, secret, {
+    subject: String(user.id),
+    expiresIn
+   })
+
+   return res.json({ user, token })
   }
 }
 
